f62274e40b96749ec2751f083eb667cee7631766
fortblocks <chris@ridiculouscorp.com>
2026-09-17 22:46:50 +0100

    Add Control-Option-M hotkey service

---
+++ macos/MeetingMode/MeetingMode/HotkeyService.swift
@@ -0,0 +1,53 @@
+import AppKit
+import Carbon
+
+final class HotkeyService {
+    static let shared = HotkeyService()
+
+    private var hotKeyRef: EventHotKeyRef?
+    private var handler: EventHandlerRef?
+    private var action: (() -> Void)?
+
+    func register(action: @escaping () -> Void) {
+        unregister()
+        self.action = action
+
+        var spec = EventTypeSpec(eventClass: OSType(kEventClassKeyboard), eventKind: UInt32(kEventHotKeyPressed))
+        let userData = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
+        InstallEventHandler(GetApplicationEventTarget(), { _, event, userData in
+            guard let userData else { return noErr }
+            let service = Unmanaged<HotkeyService>.fromOpaque(userData).takeUnretainedValue()
+            var hotKeyID = EventHotKeyID()
+            GetEventParameter(
+                event,
+                EventParamName(kEventParamDirectObject),
+                EventParamType(typeEventHotKeyID),
+                nil,
+                MemoryLayout<EventHotKeyID>.size,
+                nil,
+                &hotKeyID
+            )
+            if hotKeyID.id == 1 {
+                DispatchQueue.main.async { service.action?() }
+            }
+            return noErr
+        }, 1, &spec, userData, &handler)
+
+        var hotKeyID = EventHotKeyID(signature: OSType(0x4D4D4F44), id: 1)
+        RegisterEventHotKey(
+            UInt32(kVK_ANSI_M),
+            UInt32(controlKey + optionKey),
+            hotKeyID,
+            GetApplicationEventTarget(),
+            0,
+            &hotKeyRef
+        )
+    }
+
+    func unregister() {
+        if let hotKeyRef { UnregisterEventHotKey(hotKeyRef) }
+        if let handler { RemoveEventHandler(handler) }
+        hotKeyRef = nil
+        handler = nil
+    }
+}
